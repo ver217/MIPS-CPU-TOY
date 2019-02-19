e372054b4704096fef4c2c9d229010860219636b
Faraway <hzylovelyl@gmail.com>
2019-02-19 19:59:20 +0800

    ip core

--- cpu.srcs/sources_1/new/top.v
+++ cpu.srcs/sources_1/new/top.v
@@ -69,10 +69,17 @@ module top(
 		.data_out(memOut)
 	);
 
-	rom imem(
-		.address(pc), 
-//		.clk(clkin), 
-		.data_out(inst)
+//	rom imem(
+//		.address(pc), 
+////		.clk(clkin), 
+//		.data_out(inst)
+//	 );
+	 
+	 blk_mem_gen_0 rom(
+	       .clka(clkin),
+	       .ena(1),
+	       .addra(pc[11:0] >> 2),
+	       .douta(inst)
 	 );
 
 	regFile regfile(
