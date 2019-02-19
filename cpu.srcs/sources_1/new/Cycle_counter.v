b6e2b66c7fcc1e1fc74e03a5998e64a5826ea9aa
ver217 <lhx0217@gmail.com>
2019-02-19 18:28:41 +0800

    feat: add display module

---
+++ cpu.srcs/sources_1/new/Cycle_counter.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+//周期计数 by 史凯波
+
+module Cycle_counter(
+input clk,
+input pause,
+input reset,
+output reg [31:0]cycle_number
+    );
+
+initial begin
+cycle_number=0;
+end
+
+always@(posedge clk)begin
+if(pause==0) cycle_number=cycle_number;
+else if(reset==0) cycle_number=0;
+else cycle_number=cycle_number+1;
+end
+
+endmodule
---
+++ cpu.srcs/sources_1/new/eight_devide.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//Æß¶ÎÏÔÊ¾Æ÷É¨ÃèÄ£¿é by Ê·¿­²¨
+module eight_devide(clk,devide);
+ input clk;
+ output reg [2:0]devide;
+ 
+ reg [31:0]count;
+ reg clk_M;
+initial begin
+devide=3'h0;
+clk_M=0;
+end
+ 
+always @ (posedge clk) begin
+    count = count + 1;
+    if(count > 50000)  begin
+        clk_M = ~clk_M;
+        count = 0;
+    end
+end 
+ 
+   
+always @(posedge clk_M)  begin    // Ê±ÖÓÉÏÉıÑØ     
+	devide=devide+1;
+	end
+endmodule
